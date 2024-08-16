5d5d864c3c1b1cb83a35085797443bbc631a7621
Joel <98978800+JoelSRangel@users.noreply.github.com>
2024-08-15 23:53:28 -0300

    Adicionando correções e os documentos do modulo_2

---
+++ docs/Modulo_2/DDL.sql
@@ -0,0 +1,10 @@
+-- --------------------------------------------------------------------------------------
+-- Data Criacao ...........: 15/08/2024                                                --
+-- Autor(es) ..............: Joel Soares                                            --
+-- Versao ..............: 1.0                                                          --
+-- Banco de Dados .........: PostgreSQL                                                --
+-- Descricao .........: Inclusão de CREATE TABLE de todas as tabelas do banco de dados.--
+-- --------------------------------------------------------------------------------------
+
+BEGIN TRANSACTION; 
+
