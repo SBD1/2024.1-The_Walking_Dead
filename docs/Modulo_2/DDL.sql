67d7613c370e1498b6945290c08dbf2656a52bec
DanRocha18 <daniel.rocha1207@gmail.com>
2024-08-18 22:07:38 -0300

    Adicionando as entidades Personagem, Zumbi, Habilidades e NPC ao DDL

--- docs/Modulo_2/DDL.sql
+++ docs/Modulo_2/DDL.sql
@@ -1,19 +1,17 @@
--- --------------------------------------------------------------------------------------
--- Data Criacao ...........: 15/08/2024                                                --
--- Autor(es) ..............: Joel Soares                                            --
--- Versao ..............: 1.0                                                          --
--- Banco de Dados .........: PostgreSQL                                                --
--- Descricao .........: Inclusão de CREATE TABLE de todas as tabelas do banco de dados.--
--- --------------------------------------------------------------------------------------
-
 BEGIN TRANSACTION; 
+
+-- Criação do tipo ENUM para o campo 'Tipo' da tabela Item
+CREATE TYPE tipo_item AS ENUM ('equipamento', 'consumivel');
+
+-- Criação do tipo ENUM para o campo 'Tipo' da tabela Item_Equipamento
+CREATE TYPE tipo_equipamento AS ENUM ('Arma', 'ferramenta', 'vestimenta', 'alimento', 'medicamento');
 
 -- Tabela: Item
 CREATE TABLE Item (
     ID INT PRIMARY KEY,
     nome VARCHAR(60) NOT NULL,
     Descricao VARCHAR(200) NOT NULL,
-    Tipo ENUM('equipamento', 'consumivel') NOT NULL,
+    Tipo tipo_item NOT NULL,
     Valor INT CHECK(Valor >= 1 AND Valor <= 5000) -- Permitido NULL por padrão
 );
 
@@ -23,6 +21,27 @@ CREATE TABLE Instancia_Item (
     Item_ID INT NOT NULL,
     Localizacao VARCHAR(60),
     FOREIGN KEY (Item_ID) REFERENCES Item(ID)
+);
+
+-- Tabela: Habilidades
+CREATE TABLE Habilidades (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(60) NOT NULL,
+    Descricao VARCHAR(5000) NOT NULL
+);
+
+-- Tabela: Personagem (Referência à tabela Habilidades)
+CREATE TABLE Personagem (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(60) NOT NULL,
+    Genero VARCHAR(50) NOT NULL,
+    Forca INT CHECK(Forca >= 0) NOT NULL,
+    Velocidade INT CHECK(Velocidade >= 0) NOT NULL,
+    HP INT CHECK(HP >= 0) NOT NULL,
+    Habilidades_ID INT,
+    Status VARCHAR(60) NOT NULL,
+    Localizacao VARCHAR(60) NOT NULL,
+    FOREIGN KEY (Habilidades_ID) REFERENCES Habilidades(ID)
 );
 
 -- Tabela: Inventário
@@ -39,7 +58,7 @@ CREATE TABLE Inventario (
 CREATE TABLE Item_Equipamento (
     ID INT PRIMARY KEY,
     Estado VARCHAR(20) NOT NULL,
-    Tipo ENUM('Arma', 'ferramenta', 'vestimenta', 'alimento', 'medicamento') NOT NULL,
+    Tipo tipo_equipamento NOT NULL,
     FOREIGN KEY (ID) REFERENCES Item(ID)
 );
 
@@ -62,4 +81,19 @@ CREATE TABLE Item_Consumivel (
     ID INT PRIMARY KEY,
     Poder_de_Regeneracao INT CHECK(Poder_de_Regeneracao >= 0 AND Poder_de_Regeneracao <= 5000) NOT NULL,
     FOREIGN KEY (ID) REFERENCES Item(ID)
+);
+
+-- Tabela: Zumbi
+CREATE TABLE Zumbi (
+    ID INT PRIMARY KEY,
+    Forca INT CHECK(Forca >= 0) NOT NULL,
+    Velocidade INT CHECK(Velocidade >= 0) NOT NULL,
+    HP INT CHECK(HP >= 0) NOT NULL
+);
+
+-- Tabela: NPC
+CREATE TABLE NPC (
+    ID INT PRIMARY KEY,
+    Funcao VARCHAR(60) NOT NULL,
+    Dialogo VARCHAR(5000) NOT NULL
 );
