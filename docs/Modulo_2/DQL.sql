fedfe2bc88a9fe7cf005c9fdf241bd0b94b34228
matheusfga <matheusrfga@gmail.com>
2024-08-19 22:44:32 -0300

    Adding ;

--- docs/Modulo_2/DQL.sql
+++ docs/Modulo_2/DQL.sql
@@ -66,4 +66,4 @@ SELECT ID, Item_ID FROM Instancia_Item WHERE Localização = 'Prisão';
 
 -- Mostra o ID e nome de todos os itens que são premio de alguma missão 
 SELECT ID, nome FROM Item, Missao
-WHERE Item.ID = Missao.premio
+WHERE Item.ID = Missao.premio;
