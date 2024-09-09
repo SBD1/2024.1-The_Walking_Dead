c87f5b5de8b3417c3ee6c4054f83d6df2eae593b
JoelSRangel <joelsoaresrngl@gmail.com>
2024-09-09 20:14:13 -0300

    arrumando DDL e DML

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,128 +1,88 @@
-BEGIN TRANSACTION; 
+BEGIN TRANSACTION;
 
--- Criação do tipo ENUM para o campo 'Tipo' da tabela Item
-CREATE TYPE tipo_item AS ENUM ('equipamento', 'consumivel');
-
+CREATE TYPE tipo_item as ENUM ('equipamento', 'consumivel');
 -- Criação do tipo ENUM para o campo 'Tipo' da tabela Item_Equipamento
-CREATE TYPE tipo_equipamento AS ENUM ('Arma', 'ferramenta', 'vestimenta', 'alimento', 'medicamento');
+CREATE TYPE tipo_equipamento AS ENUM ('arma', 'armadura');
+
+CREATE TYPE parte_do_corpo AS ENUM ('cabeca', 'torso');
+
+CREATE TYPE t_genero AS ENUM ('Masculino', 'Feminino');
 
 -- Criação do tipo ENUM para o campo 'Tipo' da tabela Missao
 CREATE TYPE tipo_missao AS ENUM ('combate', 'busca', 'dialogo');
 
--- Criação do tipo ENUM para o campo 'Tipo' da tabela Local
 CREATE TYPE tipo_local AS ENUM('Cidade', 'Cadeia', 'Floresta');
 
--- Tabela: Item
 CREATE TABLE Item (
     ID INT PRIMARY KEY,
-    nome VARCHAR(60) NOT NULL,
-    Descricao VARCHAR(200) NOT NULL,
-    Tipo tipo_item NOT NULL,
-    Valor INT CHECK(Valor >= 1 AND Valor <= 5000) -- Permitido NULL por padrão
-);
-
--- Tabela: Instância_Item
-CREATE TABLE Instancia_Item (
-    ID INT PRIMARY KEY,
-    Item_ID INT NOT NULL,
-    Localizacao VARCHAR(60),
-    FOREIGN KEY (Item_ID) REFERENCES Item(ID)
-);
-
--- Tabela: Habilidades
-CREATE TABLE Habilidades (
-    ID INT PRIMARY KEY,
-    Nome VARCHAR(60) NOT NULL,
-    Descricao VARCHAR(5000) NOT NULL
-);
-
--- Criando a tabela Personagem
-CREATE TABLE Personagem (
-    ID INT PRIMARY KEY,
-    Nome VARCHAR(60) NOT NULL,
-    Genero VARCHAR(50) NOT NULL,
-    HP INT CHECK(HP >= 0) NOT NULL,
-    Status VARCHAR(60) NOT NULL,
-    Localizacao VARCHAR(60) NOT NULL
-);
-
--- Tabela: Inventário
-CREATE TABLE Inventario (
-    ID INT PRIMARY KEY,
-    Personagem_ID INT NOT NULL,
-    Tamanho INT CHECK(Tamanho >= 1 AND Tamanho <= 50) NOT NULL,
-    FOREIGN KEY (Personagem_ID) REFERENCES Personagem(ID)
-);
-
-CREATE TABLE inventario_item (
-    ID_inventario INT,
-    ID_item INT,
-    PRIMARY KEY (ID_inventario, ID_item),
-    FOREIGN KEY (ID_inventario) REFERENCES inventario(ID),
-    FOREIGN KEY (ID_item) REFERENCES Instancia_Item(ID)
+    nome VARCHAR(30) NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
+    tipo tipo_item NOT NULL,
+    valor INT
 );
 
 -- Tabela: Item-Equipamento
 CREATE TABLE Item_Equipamento (
     ID INT PRIMARY KEY,
-    Estado VARCHAR(20) NOT NULL,
-    Tipo tipo_equipamento NOT NULL,
-    FOREIGN KEY (ID) REFERENCES Item(ID)
+    estado VARCHAR(20) NOT NULL,
+    durabilidade INT,
+    tipo tipo_equipamento NOT NULL,
+    FOREIGN KEY (ID) REFERENCES Item(ID)    
 );
 
 -- Tabela: Item-Equipamento-Arma
 CREATE TABLE Item_Equipamento_Arma (
     ID INT PRIMARY KEY,
-    Poder_de_Ataque INT CHECK(Poder_de_Ataque >= 0 AND Poder_de_Ataque <= 5000) NOT NULL,
+    poder_de_ataque INT NOT NULL,
+    municao INT,
     FOREIGN KEY (ID) REFERENCES Item_Equipamento(ID)
 );
 
 -- Tabela: Item-Equipamento-Armadura
 CREATE TABLE Item_Equipamento_Armadura (
     ID INT PRIMARY KEY,
-    Poder_de_Defesa INT CHECK(Poder_de_Defesa >= 0 AND Poder_de_Defesa <= 5000) NOT NULL,
+    poder_de_defesa INT NOT NULL,
+    protege parte_do_corpo NOT NULL, 
     FOREIGN KEY (ID) REFERENCES Item_Equipamento(ID)
 );
 
 -- Tabela: Item-Consumível
 CREATE TABLE Item_Consumivel (
     ID INT PRIMARY KEY,
-    Poder_de_Regeneracao INT CHECK(Poder_de_Regeneracao >= 0 AND Poder_de_Regeneracao <= 5000) NOT NULL,
+    poder_de_regeneracao INT NOT NULL,
     FOREIGN KEY (ID) REFERENCES Item(ID)
 );
 
--- Tabela: Zumbi
-CREATE TABLE Zumbi (
+-- Tabela: Habilidades
+CREATE TABLE Habilidades (
     ID INT PRIMARY KEY,
-    Forca INT CHECK(Forca >= 0) NOT NULL,
-    Velocidade INT CHECK(Velocidade >= 0) NOT NULL,
-    HP INT CHECK(HP >= 0) NOT NULL
+    nome VARCHAR(50) NOT NULL,
+    descricao VARCHAR(300) NOT NULL
 );
 
--- Criando a tabela Instancia_Zumbi
-CREATE TABLE Instancia_Zumbi (
-    ID_Instancia SERIAL PRIMARY KEY,  -- ID único para cada instância de zumbi
-    ID_Zumbi INT NOT NULL,            -- Referência ao zumbi na tabela Zumbi
-    Status VARCHAR(60) NOT NULL,
-    Localizacao VARCHAR(60) NOT NULL,
-    FOREIGN KEY (ID_Zumbi) REFERENCES Zumbi(ID)
+CREATE TABLE Personagem (
+    ID INT PRIMARY KEY,
+    nome VARCHAR(60) NOT NULL,
+    genero t_genero NOT NULL,
+    hp INT CHECK(HP >= 0) NOT NULL,
+    estado VARCHAR(60) NOT NULL,
+    localizacao VARCHAR(60) NOT NULL
 );
 
--- Criando a tabela Jogador com referência à tabela Personagem
 CREATE TABLE Jogador (
     ID INT PRIMARY KEY,
-    Forca INT CHECK(Forca >= 0) NOT NULL,
-    Velocidade INT CHECK(Velocidade >= 0) NOT NULL,
-    Habilidades_ID INT,
+    forca INT CHECK(forca >= 0) NOT NULL,
+    agilidade INT CHECK(agilidade >= 0) NOT NULL,
+    habilidades_ID INT,
     FOREIGN KEY (ID) REFERENCES Personagem(ID),
-    FOREIGN KEY (Habilidades_ID) REFERENCES Habilidades(ID)
+    FOREIGN KEY (habilidades_ID) REFERENCES Habilidades(ID)
 );
 
 -- Criando a tabela NPC com referência à tabela Personagem
 CREATE TABLE NPC (
     ID INT PRIMARY KEY,
-    Funcao VARCHAR(60) NOT NULL,
-    Dialogo VARCHAR(5000) NOT NULL,
+    funcao VARCHAR(60) NOT NULL,
+    dialogo VARCHAR(5000) NOT NULL,
     FOREIGN KEY (ID) REFERENCES Personagem(ID)
 );
 
@@ -139,16 +99,57 @@ CREATE TABLE Missao (
 CREATE TABLE Local (
     ID INT PRIMARY KEY,
     nome VARCHAR(60) NOT NULL,
-    dimensoes INT NOT NULL CHECK (dimensoes BETWEEN 1 AND 100000),
     tipo tipo_local NOT NULL,
     descricao VARCHAR(200),
     recursos INT CHECK (recursos BETWEEN 1 AND 99),
     dificuldade INT CHECK (dificuldade BETWEEN 1 AND 99)
 );
+
 CREATE TABLE Regiao (
     ID INT PRIMARY KEY,
     nome VARCHAR(60) NOT NULL,
     descricao VARCHAR(200)
+);
+
+-- Tabela: Zumbi
+CREATE TABLE Zumbi (
+    ID INT PRIMARY KEY,
+    forca INT CHECK(Forca >= 0) NOT NULL,
+    velocidade INT CHECK(velocidade >= 0) NOT NULL,
+    hp INT CHECK(HP >= 0) NOT NULL
+);
+
+-- Criando a tabela Instancia_Zumbi
+CREATE TABLE Instancia_Zumbi (
+    ID_Instancia SERIAL PRIMARY KEY,  -- ID único para cada instância de zumbi
+    ID_Zumbi INT NOT NULL,            -- Referência ao zumbi na tabela Zumbi
+    estado VARCHAR(60) NOT NULL,
+    localizacao VARCHAR(60) NOT NULL,
+    FOREIGN KEY (ID_Zumbi) REFERENCES Zumbi(ID)
+);
+
+-- Tabela: Instância_Item
+CREATE TABLE Instancia_Item (
+    ID INT PRIMARY KEY,
+    Item_ID INT NOT NULL,
+    localizacao VARCHAR(60),
+    FOREIGN KEY (Item_ID) REFERENCES Item(ID)
+);
+
+-- Tabela: Inventário
+CREATE TABLE Inventario (
+    ID INT PRIMARY KEY,
+    Personagem_ID INT NOT NULL,
+    Tamanho INT CHECK(Tamanho >= 1 AND Tamanho <= 50) NOT NULL,
+    FOREIGN KEY (Personagem_ID) REFERENCES Personagem(ID)
+);
+
+CREATE TABLE Inventario_item (
+    ID_inventario INT,
+    ID_item INT,
+    PRIMARY KEY (ID_inventario, ID_item),
+    FOREIGN KEY (ID_inventario) REFERENCES Inventario(ID),
+    FOREIGN KEY (ID_item) REFERENCES Instancia_Item(ID)
 );
 
 commit;
